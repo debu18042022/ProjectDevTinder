ba00b05eec1d1dd31b41a5be997ecfb42933871b
debu18042022 <sujeetsahu322@gmail.com>
2024-10-20 13:48:33 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,33 +2,65 @@ const express = require("express");
 
 const app = express();
 
-// this will only handle Get call to /user
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Sujeet",
-    LastName: "Sahu",
-  });
-});
+// // this will only handle Get call to /user
+// // /user,/user/xyz, /user/1
+// app.get("/user", (req, res) => {
+//   res.send({firstName: "Sujeet",LastName: "Sahu",
+//   });
+// });
 
-app.post("/user", (req, res) => {
-  // data has been added to the database Logic
-  res.send("Data has been successfully added to the DB");
-});
+/* Advance Routing Techniques */
 
-app.delete("/user", (req, res) => {
-  // Data has been successfully deleted Logic
-  res.send("Data has been successfully deleted from the DB");
-});
+// // /ac, /abc  here ? means b is an optional
+// app.get("/ab?c", (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
 
-app.patch("/user", (req, res) => {
-  // Data has been successfully updated Logic
-  res.send("Data has been successfully updated in the DB");
-});
+// // /abc, /abbbc, /abbbbbbbbbbbbc, this + here means a and c should be at last and between these a and c we can add b as much as we want.
+// app.get("/ab+c", (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
 
-// this will match all the HTTP method API calls /test
-app.use("/test", (req, res) => {
-  res.send("hello from url /test url");
-});
+// // /abcd, /abSUJEETcd, /abSAhuabdjbajbajskbacd but we have to make sure it starts with ab and end with cd and in between these that can be anything eg. /abSUJEETcd, /abSAhuabdjbajbajskbacd these will perfectly work. for /abSUJEETc it won't work
+// app.get("/ab*cd", (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
+
+// WE CAN GROUP THINGS TOGETHER :
+
+// abcd ,ab it will work but abc , acd it won't work
+// app.get("/a(bc)?d", (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
+
+// // abcbcbcbcbcbcbcbcbcbcbcbcd it will work so many bc as a group b/w a and d.
+// app.get("/a(bc)+d", (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
+
+// WE CAN WRITE REGEX OVER HERE INSTEAD OF STRING
+
+// // it will work any path which contains in the url Eg. a,bad,cab,car,bacd,bacad,bcaad, etc
+// app.get(/a/, (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
+
+// // Complex Regex below: any path which end with fly it will work eg. Dragonfly,butterfly, fly etc and it won't work for Dragonfly1
+// app.get(/.*fly$/, (req, res) => {
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
+
+// //How to read query params in the routes
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
+
+// //How to read params in the dynamic routes
+// app.get("/user/:id/:name/:password", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstName: "Sujeet", LastName: "Sahu" });
+// });
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
