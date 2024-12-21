4b71b69d20d771dc93e85fbb3695c1049c67b2b5
debu18042022 <sujeetsahu322@gmail.com>
2024-12-21 17:27:58 +0530

    get request

--- src/app.js
+++ src/app.js
@@ -16,6 +16,61 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/singleuser", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length !== 0) {
+      res.status(200).send(users);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.get("/userbyid", async (req, res) => {
+  const userId = req.body._id;
+  try {
+    const user = await User.findById({ _id: userId });
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established!");
